9871d5d79b1d7341534cb30cf04281d0ffa60c11
taehyeon <97taehyun@gmail.com>
2023-12-15 15:32:33 +0900

    프론트 유효성 검사 변수명 변경, 주석추가

--- src/main/resources/static/validateForm.js
+++ src/main/resources/static/validateForm.js
@@ -5,66 +5,82 @@ function validateForm() {
     let confirmPassword = document.getElementById("confirmPassword").value.trim();
     let title = document.getElementById("title").value;
     let content = document.getElementById("content").value;
-    // let password = document.getElementById("password").value; 파일
 
-
-    /**
-     * 파라미터의 유효성 검증 후 통과하지 못할 경우 false 반환하여 form 데이터들을 서버로 보내지 않음
-     */
-    if (isNotValidCategory(category)){
+    // 유효성 검사 로직 시작 //
+    if (inValidCategory(category)){
         alert("카테고리를 선택해주세요.");
         return false;
     }
 
-    if (isNotValidWriter(writer)){
+    if (inValidWriter(writer)){
         alert("작성자는 3글자 이상 5글자 미만이어야 합니다.");
         return false;
     }
 
-    if (isNotPasswordMatching(password, confirmPassword)){
+    if (passwordMismatch(password, confirmPassword)){
         alert("비밀번호가 일치하지 않습니다. ");
         return false;
     }
 
-    if (isNotValidPassword(password)){
+    if (inValidPassword(password)){
         alert("비밀번호는 4글자 이상 16글자 미만, 영문/숫자/특수문자(@#$%^&+=) 포함되어야 합니다. ");
         return false;
     }
 
-    if (isNotValidTitle(title)){
+    if (inValidTitle(title)){
         alert("제목은 4글자 이상 100글자 미만이어야 합니다.");
         return false;
     }
 
-    if (isNotValidContent(content)){
+    if (inValidContent(content)){
         alert("내용은 4글자 이상 2000글자 미만이어야 합니다.");
         return false;
     }
+    // 유효성 검사 로직 종료 //
 
     return true;
 }
 
-
-// 카테고리를 고르지 않았으면 true 반환
-function isNotValidCategory(category) {
+/**
+ * 카테고리가 유효하지 않으면 true 반환
+ * 조건: 공백일 경우
+ * @param category
+ * @returns {boolean}
+ */
+function inValidCategory(category) {
 
     return category === "";
 }
 
-// 작성자가 공백이거나 3글자 이상 5글자 미만이면 true 반환
-function isNotValidWriter(writer) {
+/**
+ * 작성자가 유효하지 않으면 true 반환
+ * 조건: 공백이거나 3글자 이상 5글자 미만일 경우
+ * @param writer
+ * @returns {boolean}
+ */
+function inValidWriter(writer) {
 
     return writer === "" || writer.length < 3 || writer.length >= 5;
 }
 
-// 두 비밀번호가 일치하지 않으면 true 반환
-function isNotPasswordMatching(password, confirmPassword) {
+/**
+ * 두 비밀번호가 일치하지 않으면 true 반환
+ * @param password
+ * @param confirmPassword
+ * @returns {boolean}
+ */
+function passwordMismatch(password, confirmPassword) {
 
     return password !== confirmPassword;
 }
 
-// 비밀번호에 영문, 숫자, 특수문자가 포함되어 있지 않거나 길이가 4글자 이상, 16글자 미만이 아니면 true 반환
-function isNotValidPassword(password) {
+/**
+ * 비밀번호가 유효하지 않으면 true 반환
+ * 조건: 영문, 숫자, 특수문자가 포함되어 있지 않거나 길이가 4글자 이상, 16글자 미만일 경우
+ * @param password
+ * @returns {boolean}
+ */
+function inValidPassword(password) {
     if (password.length < 4 || password.length >= 16) {
         return true;
     }
@@ -78,14 +94,24 @@ function isNotValidPassword(password) {
     return !(letterPattern.test(password) && numberPattern.test(password) && specialCharPattern.test(password));
 }
 
-// 제목이 공백이거나 4글자 이상, 100글자 미만이면 true 반환
-function isNotValidTitle(title) {
+/**
+ * 제목이 유효하지 않을 경우 true 반환
+ * 조건: 공백이거나 4글자 이상, 100글자 미만일 경우
+ * @param title
+ * @returns {boolean}
+ */
+function inValidTitle(title) {
 
     return title === "" || title.length() < 4 || title.length() >= 100;
 }
 
-// 내용이 공백이거나 4글자 이상, 2000글자 미만이면 true 반환
-function isNotValidContent(content) {
+/**
+ * 내용이 유효하지 않을 경우 true 반환
+ * 조건: 공백이거나 4글자 이상, 2000글자 미만일 경우
+ * @param content
+ * @returns {boolean}
+ */
+function inValidContent(content) {
 
     return content === "" || content.length() < 4 || content.length() >= 2000;
 }
