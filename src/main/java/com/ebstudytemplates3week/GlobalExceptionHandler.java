10f77d9f22fc0c3028ee9366ab5efac48c0129c4
taehyeon <97taehyun@gmail.com>
2023-12-23 01:19:00 +0900

    글로벌 핸들러 패키지 위치 변경

--- src/main/java/com/ebstudytemplates3week/GlobalExceptionHandler.java
+++ src/main/java/com/ebstudytemplates3week/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.ebstudytemplates3week.exception;
+package com.ebstudytemplates3week;
 
+import com.ebstudytemplates3week.exception.PasswordMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
