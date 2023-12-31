2e650edcbecb60f58303cd612f0944920e98b884
taehyeon <97taehyun@gmail.com>
2024-01-01 02:05:25 +0900

    댓글 api의 Request를 vo로 받도록 변경

--- src/main/java/com/ebstudytemplates3week/restcontroller/CommentRestController.java
+++ src/main/java/com/ebstudytemplates3week/restcontroller/CommentRestController.java
@@ -4,8 +4,6 @@ import com.ebstudytemplates3week.Response.ListResponse;
 import com.ebstudytemplates3week.Response.ResponseService;
 import com.ebstudytemplates3week.service.CommentService;
 import com.ebstudytemplates3week.vo.Comment;
-import com.ebstudytemplates3week.vo.CommentResponse;
-import com.ebstudytemplates3week.vo.Pagination;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -24,37 +21,34 @@ public class CommentRestController {
     private final CommentService commentService;
     private final ResponseService responseService;
 
-    @PostMapping("/view/{id}")
-    public ResponseEntity<String> insertComment(
-            @PathVariable(name = "id") String boardId,
-            @RequestBody Map<String, String> data,
-            @ModelAttribute("pagination") Pagination pagination) {
-
-        commentService.insertComment(new Comment(boardId, data.get("content")));
-
-        // 성공적으로 댓글이 등록되었다는 JSON 응답을 클라이언트에게 전송
-        return ResponseEntity.status(HttpStatus.CREATED).body("{\"boardId\": \"" + boardId + "\"}");
-    }
-
-    @GetMapping("/comments/{id}")
+    /**
+     * 게시글 번호에 해당하는 댓글 내용 조회
+     * @param boardId 게시글 번호
+     * @return ListResponse<Comment>
+     */
+    @GetMapping("/comments/{boardId}")
     public ListResponse<Comment> getComments(
-            @PathVariable(name = "id") String boardId) {
+            @PathVariable(name = "boardId") String boardId) {
 
+        //댓글 목록 조회
         List<Comment> comments = commentService.getCommentsByBoardId(boardId);
 
         return responseService.getListResponse(comments);
     }
 
-    @PostMapping("/comments/{id}")
+    /**
+     * 게시글 번호와 댓글 내용을 입력 받아 작성하는 메서드
+     * @param boardId 게시글 번호
+     * @param comment 게시글 번호, 댓글 내용
+     * @return HttpStatus.CREATED
+     */
+    @PostMapping("/comments/{boardId}")
     public ResponseEntity<String> postComment(
-            @PathVariable(name = "id") String boardId,
-            @RequestBody CommentResponse commentResponse) {
+            @PathVariable(name = "boardId") String boardId,
+            @RequestBody Comment comment) {
 
-        log.info("commentResponse={}",commentResponse);
-
-        CommentResponse.DataObject data = commentResponse.getData();
-
-        commentService.insertComment(new Comment(data.getBoardId(), data.getContent()));
+        // 댓글 작성
+        commentService.insertComment(comment);
 
         // 성공적으로 댓글이 등록되었다는 JSON 응답을 클라이언트에게 전송
         return ResponseEntity.status(HttpStatus.CREATED).body("{\"boardId\": \"" + boardId + "\"}");
